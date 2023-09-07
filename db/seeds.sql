ebdbced65f522e23d8d2c566abc0d747e6a9bb74
Donnie Rawlings <tacobellboi@yahoo.com>
2023-09-07 10:26:36 -0500

    update seed file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,18 @@
-insert into Departments(id, name)
-values  ('1','Sales'),
-        ('2','Engineering'),
-        ('3', 'Finance'),
-        ('4', 'Legal');
+insert into Departments(name)
+values  ('Sales'),
+        ('Engineering'),
+        ('Finance'),
+        ('Legal');
 
-insert into Roles(id,title,department_id,salary)
-values  ('1', 'Sales Lead','1','100000'),
-        ('2', 'Sales Person','1','80000'),
-        ('3', 'Lead Engineer','2','150000'),
-        ('4', 'Software Engineer','2','120000'),
-        ('5', 'Account Manager','3','160000'),
-        ('6', 'Accountant','3','125000'),
-        ('7', 'Legal Team lead','4','250000'),
-        ('8', 'Lawyer','4','190000');
+insert into Roles(title,department_id,salary)
+values  ('Sales Lead','1','100000'),
+        ('Sales Person','1','80000'),
+        ('Lead Engineer','2','150000'),
+        ('Software Engineer','2','120000'),
+        ('Account Manager','3','160000'),
+        ('Accountant','3','125000'),
+        ('Legal Team lead','4','250000'),
+        ('Lawyer','4','190000');
 
 insert into Employees(first_name,last_name,role_id,manager_id)
 values  ('John','Doe','1',null),
